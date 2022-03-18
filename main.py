edb4c8e10a0c58d2d94e80ad4966663b8f33bdd5
Maxwell Tang <91094317+mackstey28@users.noreply.github.com>
2022-03-18 14:54:18 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ tree = Tree("Regular tree")
 tmp_tree = Tree("")
 
 def create_tree(curr_tree, dir) -> None:
-	if len(dir) is 0:
+	if len(dir) == 0:
 		curr_tree.add("<Empty>")
 		return
 	for x in dir:
@@ -26,8 +26,8 @@ print(tree)
 
 # ----------------- Bonus section ----------------------
 
-filter = re.compile(r"(py|txt|csv)\b") # remove whichever file extension we don't want
-									   # filter = re.compile(r"(py|txt|csv)\b")
+filter = re.compile(r"(.py|.txt|.csv)\b") # remove whichever file extension we don't want
+									      # filter = re.compile(r"(.py|.txt|.csv)\b")
 
 filtered_tree = Tree("Filtered tree")
 
@@ -37,16 +37,23 @@ def has_file_extension(n) -> bool:
 		return True
 	return False
 
+tmp_tree = Tree("")
+hjb = False;
+
 def create_filtered_tree(curr_tree, dir) -> None:
-	if len(dir) is 0:
+	if len(dir) == 0:
 		curr_tree.add("<Empty>")
 		return
 	for x in dir:
 		if type(x) is list: # directory
 			create_tree(tmp_tree, x)
 		else: # directory/file name
-			tmp_tree = curr_tree.add(x)
+			if "." in x:
+				if has_file_extension(x):
+					curr_tree.add(x)
+			else:
+				tmp_tree = curr_tree.add(x)
 
-create_tree(tree, dirs_and_files)
+create_filtered_tree(filtered_tree, dirs_and_files)
 
 print(filtered_tree)
