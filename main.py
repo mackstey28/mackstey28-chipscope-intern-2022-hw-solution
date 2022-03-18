832caa2a24a40caff12906e081d119009b0f3fb7
Maxwell Tang <91094317+mackstey28@users.noreply.github.com>
2022-03-18 02:59:19 -0400

    Update
    Tweaked a bit for creating a tree

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ print(dirs_and_files)
 tree = Tree("Regular tree")
 
 def is_file(n) -> bool:
+	if type(n) == str:
+		return False
 	if (n.endswith(".py") or n.endswith(".txt") or n.endswith(".csv")):
 		return True
 	return False
@@ -18,15 +20,17 @@ temp_tree2 = Tree("")
 
 for x in dirs_and_files:
 	if type(x) == list:
-		if len(x) == 0:
+		if len(x) == 0: # if we encounter an empty directory
 			temp_tree.add("<Empty>")
 			continue
-		for y in x:
+		for y in x: # iterate over directory contents
 			if type(y) == list:
-				if len(y) == 0:
+				if len(y) == 0: # if we encounter an empty directory in a directory
 					temp_tree2.add("<Empty>")
 					continue
-				for z in y:
+				for z in y: # iterate over directory contents in directory
+					if not is_file(z): # we will not create any more directories, so we just skip over if it is not a file
+						continue
 					temp_tree2.add(z)
 			else: # directory in directory in root
 				temp_tree2 = temp_tree.add(y)
